33568fbe0cbd5f06e569beb325d106eca513fa36
ansu1998 <ansuapillai1998@gmail.com>
2022-03-02 17:19:46 +0530

    Second

---
+++ src/com/company/Program1.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Program1 {
+
+    public static void main(String[] args) {
+        String s = "Hello";
+        s = s.concat(" World");
+        System.out.println(s);
+    }
+}
+
